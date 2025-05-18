8a44448cfce8648429100ce749312c0e334a8d48
Jean Carlo Chara Condori <115038446+JeanChara@users.noreply.github.com>
2025-05-17 19:55:29 -0500

    Inicializacion de proyecto Spring Boot con configuración base
    - Proyecto creado con Spring Initializr
    - Versión de Spring Boot: 3.4.5
    - Tipo de paquete: JAR
    - Lenguaje: Java 17
    - Group ID: org.grupo2
    - Artifact ID: spring_rmi

---
+++ Servidor/spring_rmi/src/main/java/org/grupo2/spring_rmi/SpringRmiApplication.java
@@ -0,0 +1,13 @@
+package org.grupo2.spring_rmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRmiApplication.class, args);
+	}
+
+}
---
+++ Servidor/spring_rmi/src/test/java/org/grupo2/spring_rmi/SpringRmiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grupo2.spring_rmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRmiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
