c8c9ea8ee80ce4610082d9b408f8341d1e4173cb
Jean Carlo Chara Condori <115038446+JeanChara@users.noreply.github.com>
2025-05-18 21:22:36 -0500

    Cambio de carpetas para Cliente, ahora dentro de org.grupo2

---
+++ Servidor/spring_rmi/src/main/java/org/grupo2/Cliente/CalculatorService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.grupo2.Cliente;
+
+/**
+ *
+ * @author arles
+ */
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface CalculatorService extends Remote {
+    int add(int a, int b) throws RemoteException;
+    int subtract(int a, int b) throws RemoteException;
+}
---
+++ Servidor/spring_rmi/src/main/java/org/grupo2/Cliente/CalculatorServiceClient.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.grupo2.Cliente;
+
+/**
+ *
+ * @author arles
+ */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CalculatorServiceClient {
+
+    @Autowired
+    private CalculatorService calculatorService;
+
+    public void performOperations() {
+        try {
+            int sum = calculatorService.add(5, 3);
+            int diff = calculatorService.subtract(10, 4);
+            System.out.println("Suma: " + sum);
+            System.out.println("Resta: " + diff);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
